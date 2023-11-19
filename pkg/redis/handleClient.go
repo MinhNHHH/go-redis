3046df0eaa896052d352d1e85c8075702fdeb9a0
Nguyễn Hoàng Minh <minhnh79@fpt.com>
2023-11-20 00:11:02 +0700

    implement transaction for redis

--- pkg/redis/handleClient.go
+++ pkg/redis/handleClient.go
@@ -4,16 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"strconv"
 	"strings"
 )
 
 // HandleClient handles the incoming client connection.
 func HandleClient(conn net.Conn, r *Redis) {
 	defer conn.Close()
-
 	scanner := bufio.NewScanner(conn)
+	redis := []*Redis{r} // Perform a transaction on each Redis instance in the slice
 	for scanner.Scan() {
-		res, err := handleCommand(scanner.Text(), r)
+		res, err := handleCommand(scanner.Text(), redis)
 		if err != nil {
 			conn.Write([]byte(err.Error() + "\n"))
 		} else {
@@ -22,8 +23,59 @@ func HandleClient(conn net.Conn, r *Redis) {
 	}
 }
 
+// lastInstanceRedis returns the last Redis instance in the provided slice.
+// If the slice is empty, it returns nil.
+func lastInstanceRedis(redis []*Redis) *Redis {
+	if len(redis) >= 1 {
+		return redis[len(redis)-1]
+	}
+	return nil
+}
+
+func handleGet(key string, redis []*Redis) (string, error) {
+	r := lastInstanceRedis(redis)
+	result, err := r.Get(key)
+	if err != nil {
+		return "", err
+	}
+	return result, nil
+}
+
+func handleSet(key, value string, redis []*Redis) error {
+	r := lastInstanceRedis(redis)
+	return r.Set(key, value)
+}
+
+func handleDel(key string, redis []*Redis) error {
+	r := lastInstanceRedis(redis)
+	return r.Del(key)
+}
+
+func handleLPush(key, value string, redis []*Redis) error {
+	r := lastInstanceRedis(redis)
+	return r.LPush(key, value)
+}
+
+func handleLRange(key string, start, stop int, redis []*Redis) (string, error) {
+	r := lastInstanceRedis(redis)
+	result, err := r.LRange(key, start, stop)
+	if err != nil {
+		return "", err
+	}
+	return result, nil
+}
+
+func handleLPop(key string, redis []*Redis) (string, error) {
+	r := lastInstanceRedis(redis)
+	result, err := r.LPop(key)
+	if err != nil {
+		return "", err
+	}
+	return result, nil
+}
+
 // handleCommand processes the command received from the client.
-func handleCommand(command string, r *Redis) (string, error) {
+func handleCommand(command string, redis []*Redis) (string, error) {
 	args := strings.Split(command, " ")
 	if len(args) < 2 {
 		return "", fmt.Errorf("error command")
@@ -34,7 +86,7 @@ func handleCommand(command string, r *Redis) (string, error) {
 			return "", fmt.Errorf("get command requires exactly one argument")
 		}
 		key := args[1]
-		result, err := r.Get(key)
+		result, err := handleGet(key, redis)
 		if err != nil {
 			return "", err
 		}
@@ -45,8 +97,57 @@ func handleCommand(command string, r *Redis) (string, error) {
 			return "", fmt.Errorf("set command requires at least two arguments")
 		}
 		key, value := args[1], args[2]
-		r.Set(key, value)
+		handleSet(key, value, redis)
 		return "OK", nil
+
+	case "delete":
+		if len(args) != 2 {
+			return "", fmt.Errorf("delete command requires exactly one argument")
+		}
+		key := args[1]
+		handleDel(key, redis)
+		return "OK", nil
+
+	case "lpush":
+		if len(args) < 3 {
+			return "", fmt.Errorf("lpush command requires at least two arguments")
+		}
+		key, value := args[1], args[2]
+		handleLPush(key, value, redis)
+		return "OK", nil
+
+	case "lrange":
+		if len(args) < 4 {
+			return "", fmt.Errorf("lrange command requires exactly one argument")
+		}
+		key := args[1]
+		startStr, stopStr := args[2], args[3]
+
+		start, err := strconv.Atoi(startStr)
+		if err != nil {
+			return "", err
+		}
+
+		stop, err := strconv.Atoi(stopStr)
+		if err != nil {
+			return "", err
+		}
+
+		result, err := handleLRange(key, start, stop, redis)
+		if err != nil {
+			return "", err
+		}
+		return result, nil
+	case "lpop":
+		if len(args) != 2 {
+			return "", fmt.Errorf("lpop command requires exactly one argument")
+		}
+		key := args[1]
+		result, err := handleLPop(key, redis)
+		if err != nil {
+			return "", err
+		}
+		return result, nil
 	default:
 		return "", fmt.Errorf("unknown command: %s", command)
 	}
--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -7,14 +7,16 @@ import (
 
 // DB represents a simple in-memory database.
 type Redis struct {
-	strings map[string]string // Data types Strings
-	mu      sync.Mutex        // make sure only one goroutine can access a variable at a time to avoid conflicts
+	strings map[string]string   // Data types Strings
+	lists   map[string][]string // Data types Lists
+	mu      sync.Mutex          // make sure only one goroutine can access a variable at a time to avoid conflicts
 }
 
-// NewDB creates and returns a new instance of the DB.
+// NewRedis creates and returns a new instance of the DB.
 func NewRedis() *Redis {
 	return &Redis{
 		strings: map[string]string{},
+		lists:   map[string][]string{},
 	}
 }
 
@@ -30,9 +32,63 @@ func (r *Redis) Get(key string) (string, error) {
 }
 
 // Set adds or updates a string value in the database.
-func (r *Redis) Set(key string, val string) error {
+func (r *Redis) Set(key, val string) error {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 	r.strings[key] = val
 	return nil
+}
+
+// Delete a key in the database
+func (r *Redis) Del(key string) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	delete(r.strings, key)
+	return nil
+}
+
+func (r *Redis) LPush(key, value string) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.lists[key] = append(r.lists[key], value)
+	return nil
+}
+
+func (r *Redis) LRange(key string, start int, stop int) (string, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if val, oke := r.lists[key]; oke {
+
+		if start < 0 || start > len(val) {
+			return "", fmt.Errorf("lists startIndex out of range")
+		}
+
+		if stop > len(val) {
+			return "", fmt.Errorf("lists stopIndex out of range")
+		}
+		// if stop = -1 is the last element, stop = -2 is the penultimate element of the list, and so forth.
+		if stop < 0 {
+			stop = len(val) + stop + 1
+		}
+		// If len(val) + stop + 1 < 0 => it should be return error.
+		justString := fmt.Sprint(val[start:stop])
+		return justString, nil
+	}
+	return "", fmt.Errorf("key not found")
+}
+
+func (r *Redis) LPop(key string) (string, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	if val, ok := r.lists[key]; ok {
+		element := val[len(val)-1]
+		if len(val) > 1 {
+			r.lists[key] = val[:len(val)-1]
+		} else {
+			r.lists[key] = []string{}
+		}
+		return element, nil
+	}
+	return "", fmt.Errorf("key not found")
 }
